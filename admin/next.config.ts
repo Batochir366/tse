6426c9a0b5ad64a4d661c5fb6ca8f01e3aeeb1ef
gbataa366 <gbataa366@gmail.com>
2026-04-17 19:05:12 +0800

    cors error fix

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
-
+const nextPublicApiUrl =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: [
     "192.168.1.15",
     "https://microphysical-tameka-explicitly.ngrok-free.dev",
+    nextPublicApiUrl,
   ],
   crossOrigin: "use-credentials",
 };
--- client/next.config.ts
+++ client/next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
-
+const nextPublicApiUrl =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -24,6 +25,7 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "192.168.1.15",
     "https://microphysical-tameka-explicitly.ngrok-free.dev",
+    nextPublicApiUrl,
   ],
   crossOrigin: "use-credentials",
 };
